d4d2d09b94b7731c0c10462aab2b143fff1f1c6d
Marco Vassallo <marcovassallo9@gmail.com>
2022-04-19 20:01:04 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { ListCocktailsComponent } from './activity/list-cocktails/list-cocktails.component';
+import { FavoriteCocktailsComponent } from './cocktails/favorite-cocktails/favorite-cocktails.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ListCocktailsComponent,
+    FavoriteCocktailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
